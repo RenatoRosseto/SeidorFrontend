d9eab7cddfa5eaf33a53e9802b99994d18027752
Renato Rosseto <renato_rosseto_neto@hotmail.com>
2021-03-21 12:45:50 -0300

    criando componente de filtro da home

---
+++ src/Pages/Home/api.js
@@ -0,0 +1,9 @@
+export const filtros = [
+  {value: 1, name: 'Nome'},
+  {value: 2, name: 'Cpf'},
+  {value: 3, name: 'Cargo'},
+  {value: 4, name: 'Data de cadastro'},
+  {value: 5, name: 'Uf de Nascimento'},
+  {value: 6, name: 'Faixa salarial'},
+  {value: 7, name: 'Status'},
+]
--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from '../../Components/Header'
+import {filtros} from './api'
+import Select from '../../Components/Select'
+import Input from '../../Components/Input'
+import Button from '@material-ui/core/Button';
+
+import './styles.scss'
 
 function Home() {
+  const [filtrarPor, setFiltrarPor] = useState({})
+
+  function atualizaFiltro(e) {
+    console.log(e.value)
+    setFiltrarPor(e.value)
+  }
+
   return (
     <>
       <Header titulo="Home" btnTipo="home" />
+      <div className="containerHome">
+        <div className="containerFiltros">
+          <h2>Filtros:</h2>
+          <Select titulo="Filtrar por" lista={filtros} minhaFunc={atualizaFiltro}  />
+          <Input titulo={filtrarPor.name} />
+          <Button variant="contained" color="primary">
+            Aplicar
+          </Button>
+        </div>
+      </div>
     </>
   );
 }
