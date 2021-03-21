3d2f01cea4f2cedbb4b6d629691643c94c2d8253
Renato Rosseto <renato_rosseto_neto@hotmail.com>
2021-03-21 01:09:44 -0300

    Adicionando biblioteca de componentes do Material UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import Button from '@material-ui/core/Button';
 import './global.scss'
 
 function App() {
   return (
-    <h1>Hello World</h1>
+    <Button variant="contained" color="primary">
+      Hello World
+    </Button>
   );
 }
 
